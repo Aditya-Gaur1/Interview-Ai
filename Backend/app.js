ee0534db52eb64ab951b790c793284ae3fa6ebc7
Aditya-Gaur1 <adigaur16610@gmail.com>
2026-09-19 20:03:44 +0530

    Fix production CORS configuration

--- Backend/app.js
+++ Backend/app.js
@@ -1,18 +1,23 @@
 const express = require("express");
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser()); 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}));
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
 const authRouter = require("./src/routes/auth.routes");
-const interviewRouter = require("./src/routes/interview.routes")
+const interviewRouter = require("./src/routes/interview.routes");
 
 app.use("/api/auth", authRouter);
-app.use("/api/interview" , interviewRouter);
+app.use("/api/interview", interviewRouter);
 
 module.exports = app;
